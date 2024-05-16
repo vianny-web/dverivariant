38d4631f21789399a345795147eae2a8b7cd08a7
ilya Picenko <pitsenko.ilya@mail.ru>
2024-05-16 21:14:35 +0300

    init project

---
+++ src/test/java/com/vianny/dverivariant/DverivariantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vianny.dverivariant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DverivariantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
