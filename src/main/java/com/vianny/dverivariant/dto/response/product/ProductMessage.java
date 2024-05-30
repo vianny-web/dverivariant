7e0ec36ec29bf703d08fcfdf5e84032c6bb477d5
ilya <ilya173.picenko36.95@mail.ru>
2024-05-30 20:05:01 +0300

    add: ProductMessage

---
+++ src/main/java/com/vianny/dverivariant/dto/response/product/ProductMessage.java
@@ -0,0 +1,22 @@
+package com.vianny.dverivariant.dto.response.product;
+
+import org.springframework.http.HttpStatus;
+
+
+public class ProductMessage<T> {
+    private HttpStatus httpStatus;
+    private T product;
+
+    public ProductMessage(HttpStatus httpStatus, T product) {
+        this.httpStatus = httpStatus;
+        this.product = product;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public T getProduct() {
+        return product;
+    }
+}
