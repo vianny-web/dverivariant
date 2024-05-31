337b8bf8ffceda63369444d7dce42624e008d724
ilya <ilya173.picenko36.95@mail.ru>
2024-05-31 20:04:41 +0300

    refactor(repository): move findByType to the shared repository

--- src/main/java/com/vianny/dverivariant/repositories/products/ProductsRepository.java
+++ src/main/java/com/vianny/dverivariant/repositories/products/ProductsRepository.java
@@ -1,8 +1,12 @@
 package com.vianny.dverivariant.repositories.products;
 
+import com.vianny.dverivariant.enums.TypeProducts;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.List;
+
 @NoRepositoryBean
 public interface ProductsRepository<T> extends MongoRepository<T, String> {
+    List<T> findByType(TypeProducts type);
 }
--- src/main/java/com/vianny/dverivariant/repositories/products/doors/InteriorDoorRepository.java
+++ src/main/java/com/vianny/dverivariant/repositories/products/doors/InteriorDoorRepository.java
@@ -1,11 +1,8 @@
 package com.vianny.dverivariant.repositories.products.doors;
 
-import com.vianny.dverivariant.enums.TypeProducts;
 import com.vianny.dverivariant.models.products.doors.InteriorDoor;
 import com.vianny.dverivariant.repositories.products.ProductsRepository;
 
-import java.util.List;
 
 public interface InteriorDoorRepository<T> extends ProductsRepository<InteriorDoor> {
-    List<T> findByType(TypeProducts type);
 }
