2965a8621316e3afbd88c77ce91463393f5b7016
ilya <ilya173.picenko36.95@mail.ru>
2024-06-05 18:15:42 +0300

    feat(controller): getProduct by id and type

---
+++ src/main/java/com/vianny/dverivariant/controllers/user/ProductController.java
@@ -0,0 +1,58 @@
+package com.vianny.dverivariant.controllers.user;
+
+import com.vianny.dverivariant.dto.response.message.ProductMessage;
+import com.vianny.dverivariant.dto.response.product.ProductDetailsDTO;
+import com.vianny.dverivariant.enums.TypeProducts;
+import com.vianny.dverivariant.exceptions.requiredException.ServerErrorRequiredException;
+import com.vianny.dverivariant.services.products.doors.EntranceDoorService;
+import com.vianny.dverivariant.services.products.doors.InteriorDoorService;
+import com.vianny.dverivariant.services.products.floors.LaminateService;
+import com.vianny.dverivariant.services.products.floors.QuartzvinylService;
+import com.vianny.dverivariant.services.products.others.HardwareService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog")
+public class ProductController {
+    private final InteriorDoorService interiorDoorService;
+    private final EntranceDoorService entranceDoorService;
+    private final LaminateService laminateService;
+    private final QuartzvinylService quartzvinylService;
+    private final HardwareService hardwareService;
+
+    @Autowired
+    public ProductController(InteriorDoorService interiorDoorService, EntranceDoorService entranceDoorService, LaminateService laminateService, QuartzvinylService quartzvinylService, HardwareService hardwareService) {
+        this.interiorDoorService = interiorDoorService;
+        this.entranceDoorService = entranceDoorService;
+        this.laminateService = laminateService;
+        this.quartzvinylService = quartzvinylService;
+        this.hardwareService = hardwareService;
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<ProductMessage<ProductDetailsDTO>> getProduct(@RequestParam String id, @RequestParam TypeProducts type) {
+        try {
+            ProductDetailsDTO productById = null;
+
+            switch (type) {
+                case INTERIOR_DOOR -> productById = interiorDoorService.getProductById(id);
+                case ENTRANCE_DOOR -> productById = entranceDoorService.getProductById(id);
+                case LAMINATE -> productById = laminateService.getProductById(id);
+                case QUARTZVINYL -> productById = quartzvinylService.getProductById(id);
+                case HARDWARE -> productById = hardwareService.getProductById(id);
+            }
+            
+            ProductMessage<ProductDetailsDTO> dataObject = new ProductMessage<>(HttpStatus.FOUND, productById);
+            return new ResponseEntity<>(dataObject,HttpStatus.OK);
+        }
+        catch (Exception e) {
+            throw new ServerErrorRequiredException(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/vianny/dverivariant/controllers/user/doors/InteriorDoorController.java
+++ src/main/java/com/vianny/dverivariant/controllers/user/doors/InteriorDoorController.java
@@ -2,7 +2,6 @@ package com.vianny.dverivariant.controllers.user.doors;
 
 import com.vianny.dverivariant.dto.response.message.ProductMessage;
 import com.vianny.dverivariant.dto.response.product.ProductBriefDTO;
-import com.vianny.dverivariant.dto.response.product.ProductDetailsDTO;
 import com.vianny.dverivariant.enums.TypeProducts;
 import com.vianny.dverivariant.exceptions.requiredException.ServerErrorRequiredException;
 import com.vianny.dverivariant.services.products.doors.InteriorDoorService;
@@ -11,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,18 +28,6 @@ public class InteriorDoorController {
         try {
             List<ProductBriefDTO> productBriefDTOS = interiorDoorService.getAllProductsByType(TypeProducts.INTERIOR_DOOR);
             ProductMessage<List<ProductBriefDTO>> dataObject = new ProductMessage<>(HttpStatus.FOUND, productBriefDTOS);
-            return new ResponseEntity<>(dataObject,HttpStatus.OK);
-        }
-        catch (Exception e) {
-            throw new ServerErrorRequiredException(e.getMessage());
-        }
-    }
-
-    @GetMapping("/product")
-    public ResponseEntity<ProductMessage<ProductDetailsDTO>> getProduct(@RequestParam String id) {
-        try {
-            ProductDetailsDTO productById = interiorDoorService.getProductById(id);
-            ProductMessage<ProductDetailsDTO> dataObject = new ProductMessage<>(HttpStatus.FOUND, productById);
             return new ResponseEntity<>(dataObject,HttpStatus.OK);
         }
         catch (Exception e) {
