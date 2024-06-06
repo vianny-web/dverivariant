8d08caccb17dc3f1fd8e316622207fa015a071db
ilya <ilya173.picenko36.95@mail.ru>
2024-06-06 20:52:20 +0300

    add(service): caching the image

--- src/main/java/com/vianny/dverivariant/controllers/user/ImageController.java
+++ src/main/java/com/vianny/dverivariant/controllers/user/ImageController.java
@@ -2,9 +2,12 @@ package com.vianny.dverivariant.controllers.user;
 
 import com.vianny.dverivariant.exceptions.requiredException.ServerErrorRequiredException;
 import com.vianny.dverivariant.services.minio.ImageTransferService;
+import com.vianny.dverivariant.services.redis.RedisImageService;
 import io.minio.errors.*;
+import org.apache.commons.compress.utils.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -20,21 +23,39 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/catalog")
 public class ImageController {
     private static final Logger log = LogManager.getLogger(ImageController.class);
     private ImageTransferService imageTransferService;
+    private RedisImageService redisImageService;
+
     @Autowired
     public void setFileTransferService(ImageTransferService imageTransferService) {
         this.imageTransferService = imageTransferService;
+    }
+    @Autowired
+    public void setRedisImageService(RedisImageService redisImageService) {
+        this.redisImageService = redisImageService;
     }
 
     @GetMapping("/image/")
     public ResponseEntity<Resource> downloadImage(@RequestParam String pathImage) {
         try {
-            InputStreamResource resource = imageTransferService.downloadFile(pathImage);
+            byte[] imageData;
+
+            if (redisImageService.getData(pathImage) != null) {
+                imageData = redisImageService.getData(pathImage);
+            } else {
+                InputStreamResource imageStream = imageTransferService.downloadFile(pathImage);
+                imageData = IOUtils.toByteArray(imageStream.getInputStream());
+
+                redisImageService.deleteData(pathImage);
+                redisImageService.saveData(pathImage, imageData, 5, TimeUnit.MINUTES);
+            }
+
 
             HttpHeaders downloadHeaders = new HttpHeaders();
             downloadHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
@@ -42,7 +63,7 @@ public class ImageController {
 
             return ResponseEntity.ok()
                     .headers(downloadHeaders)
-                    .body(resource);
+                    .body(new ByteArrayResource(imageData));
         } catch (IOException | ErrorResponseException | InsufficientDataException | InternalException |
                  InvalidKeyException | InvalidResponseException | NoSuchAlgorithmException | ServerException |
                  XmlParserException e) {
